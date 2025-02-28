e7488e0204ec3c697aefbfe354af77fd57ffe2a4
David Feldman <dodya.feldman@gmail.com>
2025-02-28 08:34:08 -0800

    NDC draw line algorithm

--- src/gfx.rs
+++ src/gfx.rs
@@ -1,6 +1,6 @@
 use pixels::{Pixels, SurfaceTexture};
 use winit::{
-    dpi::LogicalSize,
+    dpi::PhysicalSize,
     event::{Event, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
@@ -24,7 +24,7 @@ impl Gfx {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new()
             .with_title("graphics")
-            .with_inner_size(LogicalSize::new(width, height))
+            .with_inner_size(PhysicalSize::new(width, height))
             .with_resizable(false)
             .build(&event_loop)
             .unwrap();
@@ -49,35 +49,94 @@ impl Gfx {
         }
     }
 
-    /// Run the event loop (Handles everything: input, rendering, updates)
-    pub fn run(self) {
-        let mut pixels = self.pixels; // Move pixels into the closure
-        let state = self.state; // Move state into the closure
-        let window = self.window; // Move window into the closure
-
+    pub fn run(mut self) {
         self.event_loop.run(move |event, _, control_flow| {
+            println!("Event: {:?}", event);
             match &event {
                 Event::WindowEvent { event, .. } => match event {
                     WindowEvent::CloseRequested => {
                         println!("Window closed!");
                         *control_flow = ControlFlow::Exit;
                     }
-                    WindowEvent::KeyboardInput { input, .. } => {
-                        if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
-                            println!("Escape key pressed! Exiting...");
-                            *control_flow = ControlFlow::Exit;
+                    WindowEvent::CursorMoved { position, .. } => {
+                        let x = position.x as usize;
+                        let y = position.y as usize;
+
+                        if x < self.width as usize && y < self.height as usize {
+                            let index = (y * self.width as usize + x) * 4;
+                            self.state[index..index + 4].copy_from_slice(&[0, 0, 0, 255]);
                         }
                     }
                     _ => {}
                 },
                 Event::RedrawRequested(_) => {
-                    pixels.frame_mut().copy_from_slice(&state);
-                    pixels.render().unwrap();
+                    self.pixels.frame_mut().copy_from_slice(&self.state);
+                    self.pixels.render().unwrap();
                 }
                 _ => {}
             }
 
-            window.request_redraw();
+            self.window.request_redraw();
         });
+    }
+
+    fn _draw_line(&mut self, start: (i32, i32), end: (i32, i32)) {
+        let mut x1 = start.0;
+        let mut y1 = start.1;
+        let x2 = end.0;
+        let y2 = end.1;
+
+        let dx = i32::abs(x1 - x2);
+        let dy = i32::abs(y1 - y2);
+
+        let sx = if x1 < x2 { 1 } else { -1 };
+        let sy = if y1 < y2 { 1 } else { -1 };
+
+        let mut err = dx - dy;
+        let mut save_my_life = 10000;
+
+        loop {
+            if save_my_life < 0 {
+                break;
+            }
+
+            self.state[((y1 as usize * self.width as usize + x1 as usize) * 4)
+                ..((y1 as usize * self.width as usize + x1 as usize) * 4 + 4)]
+                .copy_from_slice(&[0, 0, 0, 255]);
+
+            if x1 == x2 {
+                break;
+            }
+
+            let e2 = 2 * err;
+            if dy > -e2 {
+                err -= dy;
+                x1 += sx;
+            }
+            if e2 < dx {
+                err += dx;
+                y1 += sy;
+            }
+
+            save_my_life -= 1;
+        }
+    }
+    pub fn draw_line(&mut self, start: (f32, f32), end: (f32, f32)) {
+        if f32::abs(start.0) > 1.0
+            || f32::abs(start.1) > 1.0
+            || f32::abs(end.0) > 1.0
+            || f32::abs(end.1) > 1.0
+        {
+            return;
+        }
+        let pixel_start = self.ndc_to_pixels(start);
+        let pixel_end = self.ndc_to_pixels(end);
+        self._draw_line(pixel_start, pixel_end);
+    }
+    fn ndc_to_pixels(&self, (x, y): (f32, f32)) -> (i32, i32) {
+        (
+            (x * self.width as f32 / 2.0 + self.width as f32 / 2.0) as i32,
+            (-y * self.height as f32 / 2.0 + self.height as f32 / 2.0) as i32,
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,6 @@ use crate::gfx::Gfx;
 mod gfx;
 fn main() {
     let mut g: Gfx = Gfx::new(600, 600);
-    draw(&mut g.state, g.width.try_into().unwrap());
+    g.draw_line((-0.65, -0.2), (0.88, 0.7));
     g.run();
-}
-fn draw(state: &mut Vec<u8>, width: usize) {
-    let start: Vec<i32> = vec![100, 200];
-    let end: Vec<i32> = vec![200, 400];
-
-    let mut x1 = start[0];
-    let mut y1 = start[1];
-    let x2 = end[0];
-    let y2 = end[1];
-
-    let dx = i32::abs(x1 - x2);
-    let dy = i32::abs(y1 - y2);
-
-    let sx = if x1 < x2 { 1 } else { -1 };
-    let sy = if y1 < y2 { 1 } else { -1 };
-
-    let mut err = dx - dy;
-    let mut save_my_life = 10000;
-
-    loop {
-        if save_my_life < 0 {
-            break;
-        }
-
-        state[((y1 as usize * width as usize + x1 as usize) * 4)
-            ..((y1 as usize * width as usize + x1 as usize) * 4 + 4)]
-            .copy_from_slice(&[0, 0, 0, 255]);
-
-        if x1 == x2 {
-            break;
-        }
-
-        let e2 = 2 * err;
-        if dy > -e2 {
-            err -= dy;
-            x1 += sx;
-        }
-        if e2 < dx {
-            err += dx;
-            y1 += sy;
-        }
-
-        save_my_life -= 1;
-    }
 }
